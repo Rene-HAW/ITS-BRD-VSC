307a4ba04609b7356df47fe551d4bb6192fa8a68
René Rudzki <Rene.Rudzki@haw-hamburg.de>
2026-07-26 02:29:19 +0200

    Time's up

---
+++ Programs/Interrupts/Inc/interrupts.h
@@ -0,0 +1,7 @@
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
+
+void initInterrupts(void);
+
+#endif /* INTERRUPTS_H */
+// EOF
---
+++ Programs/Interrupts/Src/interrupts.c
@@ -0,0 +1,31 @@
+#include "interrupts.h"
+#include "stm32f429xx.h"
+
+void initInterrupts(void) {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+    SYSCFG->EXTICR[0] &= ~(SYSCFG_EXTICR1_EXTI1 + SYSCFG_EXTICR1_EXTI0);
+    SYSCFG->EXTICR[0] |= (SYSCFG_EXTICR1_EXTI1_PG + SYSCFG_EXTICR1_EXTI0_PG);
+    
+    EXTI->RTSR |= (EXTI_RTSR_TR1 + EXTI_RTSR_TR0);
+    EXTI->FTSR |= (EXTI_FTSR_TR1 + EXTI_FTSR_TR0);
+    EXTI->IMR  |= (EXTI_IMR_MR1 + EXTI_IMR_MR0);
+
+    NVIC_SetPriority(EXTI1_IRQn, 0);
+    NVIC_SetPriority(EXTI0_IRQn, 0);
+    NVIC_EnableIRQ(EXTI1_IRQn);
+    NVIC_EnableIRQ(EXTI0_IRQn);
+}
+
+void EXTI1_IRQHandler(void) {
+    EXTI->PR = EXTI_PR_PR1;
+
+}
+
+void EXTI0_IRQHandler(void) {
+    EXTI->PR = EXTI_PR_PR1;
+
+}
+
+// EOF
